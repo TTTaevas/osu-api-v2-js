0774e03ce96a2cfbbdd02a619018fd26d7b4bdac
Taevas <code@taevas.xyz>
2025-09-27 20:55:07 +0200

    Unify `refreshToken()` & `getAndSetToken()` into `setNewToken()`
    This is not expected to change any logic, it's mere simplification
    It takes advantage of changes of recent commits to make the flow of setting the first token and refreshing the token one and the same
    (keeping in mind that the constructor is expected to be the first caller)

--- lib/index.ts
+++ lib/index.ts
@@ -80,7 +80,7 @@ export class APIError {
 
 /** An API instance is needed to make requests to the server! */
 export class API {
-	// CLIENT CREATION
+	// CLIENT CREATION & TOKENS
 
 	/** If you have the credentials for a client and do not wish to act on behalf of a user, you might want to use this constructor */
 	constructor(client_id: API["client_id"], client_secret: API["client_secret"], settings?: Partial<API>);
@@ -90,9 +90,9 @@ export class API {
 	constructor(access_token: API["access_token"], settings?: Partial<API>);
 	constructor(client_id_or_access_token: API["client_id"] | API["access_token"], client_secret_or_settings?: API["client_secret"] | Partial<API>,
 	redirect_uri_or_settings?: string | Partial<API>, code?: string, settings?: Partial<API>) {
-		this.is_refreshing_token = true
 		const actual_settings = settings ?? typeof redirect_uri_or_settings === "string" ?
 			typeof client_secret_or_settings === "string" ? undefined : client_secret_or_settings : redirect_uri_or_settings
+
 		if (actual_settings) {
 			/** delete every property that is `undefined` so the class defaults aren't overwritten by `undefined` */
 			Object.keys(actual_settings).forEach((key) => {
@@ -106,14 +106,41 @@ export class API {
 			this.client_secret = client_secret_or_settings
 
 			const promise = typeof redirect_uri_or_settings === "string" && code ?
-				this.getAndSetToken({client_id: client_id_or_access_token, client_secret: client_secret_or_settings, grant_type: "authorization_code", redirect_uri: redirect_uri_or_settings, code}) :
-				this.getAndSetToken({client_id: client_id_or_access_token, client_secret: client_secret_or_settings, grant_type: "client_credentials", scope: "public"})
+				this.setNewToken({redirect_uri: redirect_uri_or_settings, code}) :
+				this.setNewToken()
 
 			promise.then(() => this.log(false, "Set a first token!"))
-			promise.finally(() => this.is_refreshing_token = false)
-		} else {
-			this.is_refreshing_token = false
 		}
+	}
+
+	private _access_token: string = ""
+	/** The key that allows you to talk with the API */
+	get access_token() {return this._access_token}
+	set access_token(token) {this._access_token = token}
+
+	private _refresh_token?: string
+	/**
+	 * Valid for an unknown amount of time, it allows you to get a new token without going through the Authorization Code Grant again!
+	 * Use {@link API.refreshToken} to make use of this token
+	 * @remarks There is no refresh_token if the Authorization Code Grant hasn't been done, it would be pointless to have one in that case
+	 */
+	get refresh_token() {return this._refresh_token}
+	set refresh_token(token) {
+		this._refresh_token = token
+		this.updateRefreshTokenTimer() // because the refresh token may be specified last
+	}
+
+	private _token_type: string = "Bearer"
+	/** Should always be "Bearer" */
+	get token_type() {return this._token_type}
+	set token_type(token) {this._token_type = token}
+
+	private _expires: Date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000) // 24 hours default, is set through getAndSetToken anyway
+	/** The expiration date of your access_token */
+	get expires() {return this._expires}
+	set expires(date) {
+		this._expires = date
+		this.updateRefreshTokenTimer()
 	}
 
 
@@ -166,120 +193,71 @@ export class API {
 	set user(user) {this._user = user}
 
 
-	// CLIENT CONFIGURATION
+	// TOKEN HANDLING
 
-	private _verbose?: "none" | "errors" | "all" = "none"
-	/** Which events should be logged (defaults to **none**) */
-	get verbose() {return this._verbose}
-	set verbose(verbose) {this._verbose = verbose}
-
-	private _timeout: number = 20
-	/**
-	 * The maximum **amount of seconds** requests should take before returning an answer (defaults to **20**)
-	 * @remarks 0 means no maximum, no timeout
-	 */
-	get timeout() {return this._timeout}
-	set timeout(timeout) {this._timeout = timeout}
-
-	private _retry_delay: number = 2
-	/** In seconds, how long should it wait after a request failed before retrying? (defaults to **2**) */
-	get retry_delay() {return this._retry_delay}
-	set retry_delay(retry_delay) {this._retry_delay = retry_delay}
-
-	private _retry_maximum_amount: number = 4
-	/** 
-	 * How many retries maximum before throwing an {@link APIError} (defaults to **4**)
-	 * @remarks Pro tip: Set that to 0 to **completely** disable retries!
-	 */
-	get retry_maximum_amount() {return this._retry_maximum_amount}
-	set retry_maximum_amount(retry_maximum_amount) {this._retry_maximum_amount = retry_maximum_amount}
-
-	private _retry_on_automatic_token_refresh: boolean = true
-	/** Should it retry a request upon successfully refreshing the token due to {@link API.refresh_token_on_401} being `true`? (defaults to **true**) */
-	get retry_on_automatic_token_refresh() {return this._retry_on_automatic_token_refresh}
-	set retry_on_automatic_token_refresh(retry_on_automatic_token_refresh) {this._retry_on_automatic_token_refresh = retry_on_automatic_token_refresh}
-
-	private _retry_on_status_codes: number[] = [429]
-	/** Upon failing a request and receiving a response, because of which received status code should the request be retried? (defaults to **[429]**) */
-	get retry_on_status_codes() {return this._retry_on_status_codes}
-	set retry_on_status_codes(retry_on_status_codes) {this._retry_on_status_codes = retry_on_status_codes}
-
-	private _retry_on_timeout: boolean = false
-	/** Should it retry a request if that request failed because it has been aborted by the {@link API.timeout}? (defaults to **false**) */
-	get retry_on_timeout() {return this._retry_on_timeout}
-	set retry_on_timeout(retry_on_timeout) {this._retry_on_timeout = retry_on_timeout}
-
-
-	// ACCESS TOKEN STUFF
-
-	private _access_token: string = ""
-	/** The key that allows you to talk with the API */
-	get access_token() {return this._access_token}
-	set access_token(token) {this._access_token = token}
-
-	private _token_type: string = "Bearer"
-	/** Should always be "Bearer" */
-	get token_type() {return this._token_type}
-	set token_type(token) {this._token_type = token}
-
-	private _expires: Date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000) // 24 hours default, is set through getAndSetToken anyway
-	/** The expiration date of your access_token */
-	get expires() {return this._expires}
-	set expires(date) {
-		this._expires = date
-		this.updateRefreshTokenTimer()
-	}
+	/** Has {@link API.setNewToken} been called and not yet returned anything? */
+	private is_setting_token: boolean = false
 
 	/**
-	 * Set most of an `api`'s properties, like tokens, token_type, scopes, expiration_date  
-	 * @param body An Object with the client id & secret, grant_type, and stuff that depends of the grant_type
-	 * @returns `api`, just in case, because in theory it should modify the original object
+	 * This creates a new {@link API.access_token}, alongside a new {@link API.refresh_token} if arguments are provided or if a refresh_token already exists
+	 * @remarks The API object requires a {@link API.client_id} and a {@link API.client_secret} to successfully get any token
+	 * @returns Whether or not the token has changed (this should never throw)
 	 */
-	private async getAndSetToken(body: {client_id: number, client_secret: string, grant_type: "authorization_code", redirect_uri: string, code: string}):
-	Promise<API>;
-	private async getAndSetToken(body: {client_id: number, client_secret: string, grant_type: "client_credentials", scope: "public"}): Promise<API>;
-	private async getAndSetToken(body: {client_id: number, client_secret: string, grant_type: "refresh_token", refresh_token: string}): Promise<API>;
-	private async getAndSetToken(body: {
-		client_id: number,
-		client_secret: string,
-		grant_type: "authorization_code" | "client_credentials" | "refresh_token",
-		redirect_uri?: string,
-		code?: string
-		refresh_token?: string	
-	}): Promise<API> {
-		const response = await fetch(`${this.server}/${this.route_token.join("/")}`, {
-			method: "post",
-			headers: this.headers,
-			body: JSON.stringify(body),
-			signal: this.timeout > 0 ? AbortSignal.timeout(this.timeout * 1000) : undefined
-		})
-		.catch((e) => {
-			throw new APIError("Failed to fetch a token", this.server, "post", this.route_token, body, undefined, e)
-		})
+	public async setNewToken(authorization?: {redirect_uri: string, code: string}): Promise<boolean> {
+		const old_token = this.access_token
+		this.is_setting_token = true
 
-		const json: any = await response.json()
-		if (!json.access_token) {
-			this.log(true, "Unable to obtain a token! Here's what was received from the API:", json)
-			throw new APIError("No token obtained", this.server, "post", this.route_token, body, response.status)
+		try {
+			/// Request to the server
+			const grant_type = authorization ? "authorization_code" : this.refresh_token ? "refresh_token" : "client_credentials"
+			const body = {
+				grant_type,
+				client_id: this.client_id,
+				client_secret: this.client_secret,
+				refresh_token: this.refresh_token, // may be undefined, as expected from most grant_types
+				scope: grant_type === "client_credentials" ? "public" : undefined,
+				redirect_uri: authorization?.redirect_uri,
+				code: authorization?.code,
+			}
+			const response = await fetch(`${this.server}/${this.route_token.join("/")}`, {
+				method: "post",
+				headers: this.headers,
+				body: JSON.stringify(body),
+				signal: this.timeout > 0 ? AbortSignal.timeout(this.timeout * 1000) : undefined
+			}).catch((e) => {
+				throw new APIError("Failed to fetch a token", this.server, "post", this.route_token, body, undefined, e)
+			})
+
+			/// Response parsing
+			const json: any = await response.json()
+			if (!json.access_token) {
+				this.log(true, "Unable to obtain a token! Here's what was received from the API:", json)
+				throw new APIError("No token obtained", this.server, "post", this.route_token, body, response.status)
+			}
+			this.token_type = json.token_type
+			if (json.refresh_token) {this.refresh_token = json.refresh_token}
+
+			const token = json.access_token
+			this.access_token = token
+
+			const token_payload = JSON.parse(Buffer.from(token.substring(token.indexOf(".") + 1, token.lastIndexOf(".")), "base64").toString('ascii'))
+			this.scopes = token_payload.scopes
+			if (token_payload.sub && token_payload.sub.length) {this.user = Number(token_payload.sub)}
+
+			const expiration_date = new Date()
+			expiration_date.setSeconds(expiration_date.getSeconds() + json.expires_in)
+			this.expires = expiration_date
+		} catch(e) {
+			this.log(true, "Failed to get a new token :(", e)
+		} finally {
+			this.is_setting_token = false
 		}
-		this.token_type = json.token_type
-		if (json.refresh_token) {this.refresh_token = json.refresh_token}
 
-		const token = json.access_token
-		this.access_token = token
-
-		const token_payload = JSON.parse(Buffer.from(token.substring(token.indexOf(".") + 1, token.lastIndexOf(".")), "base64").toString('ascii'))
-		this.scopes = token_payload.scopes
-		if (token_payload.sub && token_payload.sub.length) {this.user = Number(token_payload.sub)}
-	
-		const expiration_date = new Date()
-		expiration_date.setSeconds(expiration_date.getSeconds() + json.expires_in)
-		this.expires = expiration_date
-
-		return this
+		if (old_token !== this.access_token) {this.log(false, "The token has been refreshed!")}
+		return old_token !== this.access_token
 	}
 
-	/** 
+	/**
 	 * Revoke your current token! **This will revoke the {@link API.refresh_token} as well if it exists**, so use this with care
 	 * @remarks Uses {@link API.route_api} instead of {@link API.route_token}, as normally expected by the server
 	 */
@@ -288,29 +266,11 @@ export class API {
 		return await this.request("delete", ["oauth", "tokens", "current"])
 	}
 
-
-	// REFRESH TOKEN STUFF
-
-	/** In other words, are we currently busy getting a new {@link API.access_token}? */
-	private is_refreshing_token: boolean = false
-
-	private _refresh_token?: string
-	/**
-	 * Valid for an unknown amount of time, it allows you to get a new token without going through the Authorization Code Grant again!
-	 * Use {@link API.refreshToken} to make use of this token
-	 * @remarks There is no refresh_token if the Authorization Code Grant hasn't been done, it would be pointless to have one in that case
-	 */
-	get refresh_token() {return this._refresh_token}
-	set refresh_token(token) {
-		this._refresh_token = token
-		this.updateRefreshTokenTimer() // because the refresh token may be specified last
-	}
-	
 	private _refresh_token_on_401: boolean = true
 	/** If true, upon failing a request due to a 401, it will call {@link API.refreshToken} (defaults to **true**) */
 	get refresh_token_on_401() {return this._refresh_token_on_401}
 	set refresh_token_on_401(refresh) {this._refresh_token_on_401 = refresh}
-	
+
 	private _refresh_token_on_expires: boolean = false
 	/**
 	 * If true, the application will silently call {@link API.refreshToken} when the{@link API.access_token} is set to expire,
@@ -352,7 +312,7 @@ export class API {
 
 			this.refresh_token_timer = setTimeout(() => {
 				try {
-					this.refreshToken()
+					this.setNewToken()
 				} catch {}
 			}, ms)
 		} else if (this._refresh_token_timer) {
@@ -360,31 +320,52 @@ export class API {
 		}
 	}
 
+
+	// CLIENT CONFIGURATION
+
+	private _verbose?: "none" | "errors" | "all" = "none"
+	/** Which events should be logged (defaults to **none**) */
+	get verbose() {return this._verbose}
+	set verbose(verbose) {this._verbose = verbose}
+
+	private _timeout: number = 20
 	/**
-	 * - Uses the {@link API.refresh_token}, {@link API.client_id}, and {@link API.client_secret}
-	 * to set a new {@link API.access_token} and {@link API.refresh_token}
-	 * - Or uses the {@link API.client_id} and {@link API.client_secret} to set a new {@link API.access_token}
-	 * @returns Whether or not the token has been refreshed
+	 * The maximum **amount of seconds** requests should take before returning an answer (defaults to **20**)
+	 * @remarks 0 means no maximum, no timeout
 	 */
-	public async refreshToken(): Promise<boolean> {
-		const old_token = this.access_token
-		this.is_refreshing_token = true
+	get timeout() {return this._timeout}
+	set timeout(timeout) {this._timeout = timeout}
 
-		try {
-			if (this.refresh_token) {
-				await this.getAndSetToken({client_id: this.client_id, client_secret: this.client_secret, grant_type: "refresh_token", refresh_token: this.refresh_token})
-			} else {
-				await this.getAndSetToken({client_id: this.client_id, client_secret: this.client_secret, grant_type: "client_credentials", scope: "public"})
-			}
-			if (old_token !== this.access_token) {this.log(false, "The token has been refreshed!")}
-		} catch(e) {
-			this.log(true, "Failed to refresh the token :(", e)
-		} finally {
-			this.is_refreshing_token = false
-		}
 
-		return old_token !== this.access_token
-	}
+	// RETRIES
+
+	private _retry_maximum_amount: number = 4
+	/**
+	 * How many retries maximum before throwing an {@link APIError} (defaults to **4**)
+	 * @remarks Pro tip: Set that to 0 to **completely** disable retries!
+	 */
+	get retry_maximum_amount() {return this._retry_maximum_amount}
+	set retry_maximum_amount(retry_maximum_amount) {this._retry_maximum_amount = retry_maximum_amount}
+
+	private _retry_delay: number = 2
+	/** In seconds, how long should it wait after a request failed before retrying? (defaults to **2**) */
+	get retry_delay() {return this._retry_delay}
+	set retry_delay(retry_delay) {this._retry_delay = retry_delay}
+
+	private _retry_on_automatic_token_refresh: boolean = true
+	/** Should it retry a request upon successfully refreshing the token due to {@link API.refresh_token_on_401} being `true`? (defaults to **true**) */
+	get retry_on_automatic_token_refresh() {return this._retry_on_automatic_token_refresh}
+	set retry_on_automatic_token_refresh(retry_on_automatic_token_refresh) {this._retry_on_automatic_token_refresh = retry_on_automatic_token_refresh}
+
+	private _retry_on_status_codes: number[] = [429]
+	/** Upon failing a request and receiving a response, because of which received status code should the request be retried? (defaults to **[429]**) */
+	get retry_on_status_codes() {return this._retry_on_status_codes}
+	set retry_on_status_codes(retry_on_status_codes) {this._retry_on_status_codes = retry_on_status_codes}
+
+	private _retry_on_timeout: boolean = false
+	/** Should it retry a request if that request failed because it has been aborted by the {@link API.timeout}? (defaults to **false**) */
+	get retry_on_timeout() {return this._retry_on_timeout}
+	set retry_on_timeout(retry_on_timeout) {this._retry_on_timeout = retry_on_timeout}
 
 
 	// OTHER METHODS
@@ -476,9 +457,9 @@ export class API {
 				
 				if (response.status === 401) {
 					if (this.refresh_token_on_401 && !info.just_refreshed) {
-						if (!this.is_refreshing_token) {
+						if (!this.is_setting_token) {
 							this.log(true, "Server responded with status code 401, your token might have expired, I will attempt to refresh your token...")
-							if (await this.refreshToken() && this.retry_on_automatic_token_refresh) {
+							if (await this.setNewToken() && this.retry_on_automatic_token_refresh) {
 								to_retry = true
 								info.just_refreshed = true
 							}
@@ -887,7 +868,7 @@ export class ChildAPI extends API {
 	/** @hidden @deprecated use API equivalent */
 	get verbose() {return this.original.verbose}
 	/** @hidden @deprecated use API equivalent */
-	refreshToken = async () => {return await this.original.refreshToken()}
+	setNewToken = async () => {return await this.original.setNewToken()}
 	/** @hidden @deprecated use API equivalent */
 	revokeToken = async () => {return await this.original.revokeToken()}
 	/** @hidden @deprecated use API equivalent */
