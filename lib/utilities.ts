24c13cfaddcf4dd0a85f5472cb07bf692398ca67
Taevas <code@taevas.xyz>
2025-10-05 23:26:06 +0200

    Improve readability of `utilities.ts`
    Its code is fundamentally important, yet I was afraid of touching it with no good reason as it could result in something breaking
    This commit doesn't intend on changing any fundamental logic
    Still, any little change might result in some breakage, therefore it doesn't directly go on master
    Furthermore, it can be used as a foundation for more serious changes

--- lib/utilities.ts
+++ lib/utilities.ts
@@ -7,45 +7,40 @@
  */
 export function adaptParametersForGETRequests(parameters: {[k: string]: any}): {[k: string]: any} {
 	// If a parameter is an empty string or is undefined, remove it
-	for (let i = 0; i < Object.entries(parameters).length; i++) {
-		if (!String(Object.values(parameters)[i]).length || Object.values(parameters)[i] === undefined) {
-			delete parameters[Object.keys(parameters)[i]]
-			i--
+	Object.entries(parameters).forEach(([key, value]) => {
+		if (!String(value).length || value === undefined) {
+			delete parameters[key]
 		}
-	}
+	})
 
 	// If a parameter is an Array, add "[]" to its name, so the server understands the request properly
-	for (let i = 0; i < Object.entries(parameters).length; i++) {	
-		if (Array.isArray(Object.values(parameters)[i]) && !Object.keys(parameters)[i].includes("[]")) {
-			parameters[`${Object.keys(parameters)[i]}[]`] = Object.values(parameters)[i]
-			delete parameters[Object.keys(parameters)[i]]
-			i--
+	Object.entries(parameters).forEach(([key, value]) => {
+		if (Array.isArray(value) && !key.includes("[]")) {
+			parameters[`${key}[]`] = value
+			delete parameters[key]
 		}
-	}
+	})
 
 	// If a parameter is an object, add its properties in "[]" such as "cursor[id]=5&cursor[score]=36.234"
-	let parameters_to_add: {[k: string]: any} = {}
-	for (let i = 0; i < Object.entries(parameters).length; i++) {
-		const value = Object.values(parameters)[i]
+	const parameters_to_add: {[k: string]: any} = {}
+	Object.entries(parameters).forEach(([key, value]) => {
 		if (typeof value === "object" && !Array.isArray(value) && value !== null) { 
-			const main_name = Object.keys(parameters)[i]
-			for (let e = 0; e < Object.entries(value).length; e++) {
-				parameters_to_add[`${main_name}[${Object.keys(value)[e]}]`] = Object.values(value)[e]
-			}
-			delete parameters[Object.keys(parameters)[i]]
-			i--
+			Object.entries(value).forEach(([key2, value2]) => {
+				parameters_to_add[`${key}[${key2}]`] = value2
+			})
+			delete parameters[key]
 		}
-	}
-	for (let i = 0; i < Object.entries(parameters_to_add).length; i++) {
-		parameters[Object.keys(parameters_to_add)[i]] = Object.values(parameters_to_add)[i]
-	}
+	})
+	Object.entries(parameters_to_add).forEach(([key, value]) => {
+		parameters[key] = value
+	})
 
 	// If a parameter is a date, make it a string
-	for (let i = 0; i < Object.entries(parameters).length; i++) {
-		if (Object.values(parameters)[i] instanceof Date) {
-			parameters[Object.keys(parameters)[i]] = (Object.values(parameters)[i] as Date).toISOString()
+	Object.entries(parameters).forEach(([key, value]) => {
+		if (value instanceof Date) {
+			parameters[key] = value.toISOString()
 		}
-	}
+	})
 
 	return parameters
 }
@@ -53,35 +48,49 @@ export function adaptParametersForGETRequests(parameters: {[k: string]: any}): {
 /**
  * Some stuff doesn't have the right type to begin with, such as dates, which are being returned as strings, this fixes that
  * @param x Anything, but should be a string, an array that contains a string, or an object which has a string
+ * @param force_string Should `x` be as much as a string as it can? (defaults to false)
  * @returns x, but with it (or what it contains) now having the correct type
  */
 export function correctType(x: any, force_string?: boolean): any {
-	// those MUST be strings; turn the server's numbers into strings and keep the server's strings as strings
-	const bannedProperties = [
-		"name", "username", "new_user_username", "source_user_username", "previousUsername", "previous_usernames", "display_name", "github_username", "osu_username", "short_name",
-		"artist", "artist_unicode", "title", "title_unicode", "tags", "location", "interests", "occupation", "twitter", "discord",
-		"beatmap_version", "version", "display_version", "author", "raw", "bbcode", "message", "creator", "source",
-	]
+	// Apply this very function to all elements of the array, with `force_string` on if previously turned on
+	if (Array.isArray(x)) {
+		return x.map((e) => correctType(e, force_string))
+	}
+
+	// Objects have depth, we need to run this very function on all of their values
+	if (typeof x === "object" && x !== null) {
+		const keys = Object.keys(x)
+		const vals = Object.values(x)
+
+		// If a key is any of those, the value is expected to be a string, so we use `force_string` to make correctType convert them to string for us
+		const unconvertables = [
+			"name", "username", "new_user_username", "source_user_username", "previousUsername", "previous_usernames", "display_name", "github_username", "osu_username", "short_name",
+			"artist", "artist_unicode", "title", "title_unicode", "tags", "location", "interests", "occupation", "twitter", "discord",
+			"beatmap_version", "version", "display_version", "author", "raw", "bbcode", "message", "creator", "source",
+		]
+		for (let i = 0; i < keys.length; i++) {
+			x[keys[i]] = correctType(vals[i], unconvertables.some((p) => keys[i] === p))
+		}
+
+		return x
+	}
+
+	// If `force_string` is on and is applicable, convert to a string (even if already string)
 	if (force_string && typeof x !== "object") {
 		return String(x)
 	}
 
-	if (typeof x === "boolean") {
-		return x
-	} else if (/^[+-[0-9][0-9]+-[0-9]{2}-[0-9]{2}($|[ T].*)/.test(x)) {
+	// Responses by the API have their dates as strings in different formats, convert those strings to Date objects
+	if (/^[+-[0-9][0-9]+-[0-9]{2}-[0-9]{2}($|[ T].*)/.test(x)) {
+		// Before converting, force all dates into UTC+0 (to avoid differences depending of which timezone you run `new Date()` in)
 		if (/[0-9]{2}:[0-9]{2}:[0-9]{2}$/.test(x)) x += "Z"
 		if (/[0-9]{2}:[0-9]{2}:[0-9]{2}\+[0-9]{2}:[0-9]{2}$/.test(x)) x = x.substring(0, x.indexOf("+")) + "Z"
 		return new Date(x)
-	} else if (Array.isArray(x)) {
-		return x.map((e) => correctType(e, force_string))
-	} else if (!isNaN(x) && x !== "") {
-		return x === null ? null : Number(x)
-	} else if (typeof x === "object" && x !== null) {
-		const keys = Object.keys(x)
-		const vals = Object.values(x)
-		for (let i = 0; i < keys.length; i++) {
-			x[keys[i]] = correctType(vals[i], bannedProperties.some((p) => keys[i] === p))
-		}
+	}
+
+	// If the string can be converted to a number and isn't `force_string`ed, convert it (namely because of user cover id and few others)
+	if (!isNaN(x) && x !== "" && x !== null && typeof x !== "boolean") {
+		return Number(x)
 	}
 
 	return x
